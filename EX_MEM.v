d9a2dfc14d1b45b1b85171a56f47239e46a9aaa7
Devi Amrutha <163720414+l05er29@users.noreply.github.com>
2024-07-27 20:11:47 +0530

    Update EX_MEM.v

--- EX_MEM.v
+++ EX_MEM.v
@@ -1,41 +1,63 @@
 module EX_MEM(
   input clk,reset,
-  input [7:0] Result_in_alu,
-  input [7:0] writedata_in, //2 bit mux2by1 output for load operation 
-  input [4:0] Rd_in, //IDEX output address of the destnation reg
-  input memread_in,memtoreg_in,memwrite_in,regwrite_in, //IDEXX outputs control inputs 
-  
-  output reg [7:0] result_out_alu,
-  output reg [7:0] writedata_out,
-  output reg [4:0]rd,
-  output reg Memread,Memtoreg, Memwrite, Regwrite);
+  input [7:0] Result_in_alu_1,
+  input [7:0] Result_in_alu_2,
+  input [7:0] writedata_in_1, //2 bit mux2by1 output for load operation ig
+  input [7:0] writedata_in_2,
+  input [4:0] Rd_in_1, //IDEX output address of the destnation reg
+  input [4:0] Rd_in_2,
+  input memread_in1,memtoreg_in1,memwrite_in1,regwrite_in1,
+  input memread_in2,memtoreg_in2,memwrite_in2,regwrite_in2, //IDEXX outputs control inputs 
+
+  output reg [7:0] result_out_alu_1,
+  output reg [7:0] writedata_out_1,
+  output reg [4:0]rd_1,
+  output reg Memread1,Memtoreg1, Memwrite1, Regwrite1,
+   output reg [7:0] result_out_alu_2,
+  output reg [7:0] writedata_out_2,
+  output reg [4:0]rd_2,
+  output reg Memread2,Memtoreg2, Memwrite2, Regwrite2);
   
   always @ (posedge clk)
     begin
       if (reset == 1'b1)
         begin
+        
+          result_out_alu_1 <= 8'b0;
+          writedata_out_1 <= 8'b0;
+          rd_1 <= 5'b0;
           
-       //   zero <= 1'b0;
-          result_out_alu <= 8'b0;
-          writedata_out <= 8'b0;
-          rd <= 5'b0;
+          Memread1 <= 1'b0;
+          Memtoreg1 <=1'b0;
+          Memwrite1 <= 1'b0;
+          Regwrite1 <= 1'b0;
+          result_out_alu_2 <= 8'b0;
+          writedata_out_2 <= 8'b0;
+          rd_2 <= 5'b0;
           
-          Memread <= 1'b0;
-          Memtoreg <=1'b0;
-          Memwrite <= 1'b0;
-          Regwrite <= 1'b0;
+          Memread2 <= 1'b0;
+          Memtoreg2 <=1'b0;
+          Memwrite2 <= 1'b0;
+          Regwrite2 <= 1'b0;
           
         end
       else
         begin
          
-          result_out_alu <= Result_in_alu;
-          writedata_out <= writedata_in;
-          rd <= Rd_in;
-          Memread <= memread_in;
-          Memtoreg <= memtoreg_in;
-          Memwrite <= memwrite_in;
-          Regwrite <= regwrite_in;
+          result_out_alu_1 <= Result_in_alu_1;
+          writedata_out_1 <= writedata_in_1;
+          rd_1 <= Rd_in_1;
+          Memread1 <= memread_in1;
+          Memtoreg1 <= memtoreg_in1;
+          Memwrite1 <= memwrite_in1;
+          Regwrite1 <= regwrite_in1;
+                   result_out_alu_2 <= Result_in_alu_2;
+          writedata_out_2 <= writedata_in_2;
+          rd_2 <= Rd_in_2;
+          Memread2 <= memread_in2;
+          Memtoreg2 <= memtoreg_in2;
+          Memwrite2 <= memwrite_in2;
+          Regwrite2 <= regwrite_in2;
           
         end
     end
