e7a8a12c51ccfc0067a4a32a23b9846694257bb0
Mwini0-0 <135434507+Mwini0-0@users.noreply.github.com>
2024-07-27 20:28:47 +0530

    Update hazard_unit.v

--- hazard_unit.v
+++ hazard_unit.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.07.2024 02:29:21
-// Design Name: 
-// Module Name: hazard_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module hazard_unit(input branch,
 input clk,
 output reg ifidflush);
